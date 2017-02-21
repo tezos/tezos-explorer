7cd38054f34f35aca931da73f27ab266639bf3eb
arthur.breitman@gmail.com <arthur.breitman@gmail.com>
2017-02-20 20:51:37 -0800

    add more tables

--- schema.sql
+++ schema.sql
@@ -5,6 +5,21 @@
 -- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- SET FOREIGN_KEY_CHECKS=0;
 
+
+-- ---
+-- Table 'git_store'
+-- git store
+-- ---
+
+DROP TABLE IF EXISTS `git_store`;
+
+CREATE TABLE `git_store` (
+  `id` BINARY(20) NOT NULL COMMENT 'git object hash'
+  `type` ENUM NOT NULL COMMENT 'blob|tree'
+  `content` VARBINARY(MAX) NOT NULL COMMENT 'content of the blob or tree'
+) COMMENT 'a direct mapping of the irmin git store'
+
+
 -- ---
 -- Table 'raw_operation'
 -- raw operation
@@ -20,7 +35,7 @@ CREATE TABLE `raw_operation` (
 
 -- ---
 -- Table 'raw_block'
--- 
+-- raw block
 -- ---
 
 DROP TABLE IF EXISTS `raw_block`;
@@ -65,6 +80,7 @@ CREATE TABLE `block_operations` (
 may be included in several blocks';
 
 
+
 -- ---
 -- Tables prefixed by seed_ refer to the entities which are only meaningful
 -- to the seed protocol.
@@ -83,6 +99,8 @@ CREATE TABLE `seed_operations` (
   `public_key` BINARY(32) NOT NULL 'public key of the source contract''s manager',
   `fee` INTEGER NOT NULL 'fee paid to the miner',
   `counter` INTEGER NOT NULL 'counter value of the source contract',
+  -- TODO do we accept partially valid operations?
+  `valid` BOOLEAN NOT NULL DEFAULT NULL 'is the operation valid or ignored',
   PRIMARY KEY (`id`)
 ) COMMENT 'an operation as viewed by the seed protocol'
 
@@ -100,28 +118,41 @@ CREATE TABLE `seed_sub_operation` (
 ) COMMENT 'seed protocol operations are packages of multiple operations';
 
 -- ---
+-- Table 'seed_account'
+-- Account in the seed protocol
+-- ---
+
+DROP TABLE IF EXISTS `seed_account`;
+		
+CREATE TABLE `seed_account` (
+  `id` BINARY(32) NOT NULL COMMENT 'hash handle of the account',
+  `counter` INTEGER NOT NULL COMMENT 'contract counter to avoid replay attacks',      
+  `manager` BINARY(32) NOT NULL COMMENT 'manager of the contract',
+  `public_key` BINARY(32) NULL DEFAULT NULL COMMENT 'public key of the manager, if known',
+  `delegate` BINARY(32) NOT NULL COMMENT 'delegate of the contract',
+  `spendable` BOOLEAN NOT NULL COMMENT 'whether the funds are spendable',
+  `delegatable` BOOLEAN NOT NULL COMMENT 'whether the delegate may be changed',
+  `balance` INTEGER NOT NULL COMMENT 'balance in tez',
+  PRIMARY KEY (`id`,`counter`)
+) COMMENT 'a contract in the seed protocol';
+
+-- ---
 -- Table 'seed_contract'
--- 
+-- Contract attached to a seed account
 -- ---
 
 DROP TABLE IF EXISTS `seed_contract`;
-		
+
 CREATE TABLE `seed_contract` (
-  `id` BINARY(32) NOT NULL 'hash of the contract',
-  `manager` BINARY(32) NOT NULL 'manager of the contract',
-  `public_key` BINARY(32) NULL DEFAULT NULL 'public key of the manager, if known',
-  `delegate` BINARY(32) NOT NULL 'delegate of the contract',
-  `spendable` BOOLEAN NOT NULL 'whether the funds are spendable',
-  `delegatable` BOOLEAN NOT NULL 'whether the delegate may be changed',
-  `source` VARBINARY(MAX) NULL DEFAULT NULL 'source code of the smart contract, null if none'
-  -- TODO What to do about storage? It can be really big and duplicated, how do we handle that?
-  -- Maybe replicate small atomic data types to a limit and make dynamic calls to the node for large
-  -- pieces?
-  `type` VARBINARY(MAX) NULL DEFAULT NULL 'type of the attached michelson code, null if none'
-  `balance` INTEGER NOT NULL 'balance in tez'
-  `counter` INTEGER NOT NULL 'contract counter to avoid replay attacks'   
-  PRIMARY KEY (`id`)
-) COMMENT 'a contract in the seed protocol';
+  `id` BINARY(32) NOT NULL 'handle of the associated account',
+  `counter` INTEGER NOT NULL 'counter of the associated account',        
+  -- TODO set a better default than MAX for these fields
+  `source` VARBINARY(MAX) NOT NULL 'source code of the smart contract, null if none',
+  `storage` VARBINARY(MAX) NOT NULL 'storage of the smart contract, IF not too large',
+  `storage_root` BINARY(32) NOT NULL 'root hash of the storage',
+  `type` VARBINARY(MAX) NOT NULL 'type of the attached michelson code and storage'
+  PRIMARY KEY (`id`, `counter`)
+) COMMENT 'contract attached to an account'
 
 -- ---
 -- Table 'seed_transaction'
@@ -131,12 +162,39 @@ CREATE TABLE `seed_contract` (
 DROP TABLE IF EXISTS `seed_transaction`;
 		
 CREATE TABLE `seed_transaction` (
-  `id` BINARY(32) NOT NULL COMMENT 'hash of the related seed_operation',
+  `id` BINARY(32) NOT NULL COMMENT 'hash of the related seed_sub_operation',
   `amount` INTEGER NOT NULL COMMENT 'amount of the transaction',
   `destination` INTEGER NOT NULL 'destination contract',
   `input_data` VARBINARY(MAX) NULL DEFAULT NULL 'input data passed if any',
   PRIMARY KEY (`id`)
 ) COMMENT 'a transaction in the seed protocol';
+
+-- ---
+-- Table 'seed_origination'
+-- origination in the seed protocol
+-- ---
+
+DROP TABLE IF EXISTS `seed_origination`;
+		
+CREATE TABLE `seed_origination` (
+  `id` BINARY(32) NOT NULL COMMENT 'hash of the related seed_sub_operation',
+  `account` BINARY(32) NULL DEFAULT NULL COMMENT 'account created if succesful',
+  `contract` BINARY(32) NULL DEFAULT NULL COMMENT 'contract created if succesful',
+  PRIMARY KEY (`id`)
+) COMMENT 'origination of an account / contract in the seed protocol'
+
+-- ---
+-- Table 'seed_delegation'
+-- delegation in the seed protocol
+-- ---
+
+DROP TABLE IF EXISTS `seed_delegation`;
+
+CREATE TABLE `seed_delegation` (
+  `id` BINARY(32) NOT NULL COMMENT 'hash of the related seed_sub_operation',
+  `delegate` BINARY(32) NOT NULL COMMENT 'delegate key',
+  PRIMARY KEY (`id`)
+)
 
 -- ---
 -- Foreign Keys 
@@ -153,7 +211,11 @@ ALTER TABLE `seed_operations` ADD FOREIGN KEY (id) REFERENCES `raw_operation` (`
 ALTER TABLE `seed_operations` ADD FOREIGN KEY (source) REFERENCES `seed_contract` (`id`);
 ALTER TABLE `seed_sub_operation` ADD FOREIGN KEY (id) REFERENCES `seed_operation` (`id`);
 ALTER TABLE `seed_transaction` ADD FOREIGN KEY (id) REFERENCES `seed_sub_operation` (`id`);
-ALTER TABLE `seed_transaction` ADD FOREIGN KEY (destination) REFERENCES `seed_contract` (`id`);
+ALTER TABLE `seed_transaction` ADD FOREIGN KEY (destination) REFERENCES `seed_account` (`id`);
+ALTER TABLE `seed_contract` ADD FOREIGN KEY account_fk (id, counter) REFERENCES `seed_account` (`id`, `counter`);
+ALTER TABLE `seed_origination` ADD FOREIGN KEY (id) REFERENCES `seed_sub_operation` (`id`);
+ALTER TABLE `seed_origination` ADD FOREIGN KEY (account) REFERENCES `seed_account` (`id`);
+ALTER TABLE `seed_origination` ADD FOREIGN KEY (contract) REFERENCES `seed_contract` (`id`);
 
 -- ---
 -- Table Properties
@@ -188,3 +250,4 @@ ALTER TABLE `seed_transaction` ADD FOREIGN KEY (destination) REFERENCES `seed_co
 -- ('');
 -- INSERT INTO `seed_transaction` (`id`,`amount`,`destination`) VALUES
 -- ('','','');
+
